55720577d7b944ba1af91b462e0536fd4a27f30e
nnurmano <nurlan.nurmanov@gmail.com>
2017-09-14 20:28:46 +0600

    Added anchor support in the FileName component

--- src/filename.jsx
+++ src/filename.jsx
@@ -25,11 +25,17 @@ class Filename extends Component {
         console.log('The url passed is', this.props.url);
         console.log('The file name is ', this.state.filename);
         return (
-            <a href={this.props.url} alt={this.state.filename}>
-                <span className={`react-fine-uploader-filename ${this.props.className || ''}`}>
-                    {this.state.filename}
-                </span>
-            </a>
+            <div>
+                {this.props.url ?
+                    <a href={this.props.url} alt={this.state.filename}>
+                        {this.state.filename}
+                    </a>
+                    :
+                    <span className={`react-fine-uploader-filename ${this.props.className || ''}`}>
+                        {this.state.filename}
+                    </span>
+                }
+            </div>
 
         )
     }
